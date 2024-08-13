392bec5856c94d8b9e6810db0f96deac3464dd68
PrateekDahiya <dahiyaprateek27@gmail.com>
2024-08-13 21:26:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav.js";
+import Home from "./components/Home.js";
+import Confluence from "./components/Confluence.js";
+import Artworks2 from "./components/Artworks2.js";
+import Footer from "./components/Footer.js";
+import Events from "./components/Events.js";
+import Workshops from "./components/Workshops.js";
+import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <div className="nav">
+                    <Nav />
+                </div>
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/artworks" element={<Artworks2 />} />
+                        <Route path="/confluence" element={<Confluence />} />
+                        <Route path="/events" element={<Events />} />
+                        <Route path="/workshops" element={<Workshops />} />
+                    </Routes>
+                    <div className="footer">
+                        <Footer />
+                    </div>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
